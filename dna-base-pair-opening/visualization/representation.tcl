e67941f5866c858ed996aa25d8d71afd92cc9595
Viveca Lindahl <vivecalindahl@gmail.com>
2018-05-22 19:13:05 +0200

    updated vmd representation

--- dna-base-pair-opening/visualization/representation.tcl
+++ dna-base-pair-opening/visualization/representation.tcl
@@ -1,23 +1,32 @@
-# color ids
+# Some color ids
 set gray 2
 set silver 6
+set green 7
 
 set irep 0
 set imol 0
 
 # Nucleic acid backbone              
-mol modselect $irep $imol not (water or ions)
+mol modselect $irep $imol nucleic and backbone
 mol modstyle $irep $imol NewRibbons 0.300000 10.000000 3.000000 0
 mol modcolor $irep $imol ColorID $silver
 set irep [expr {$irep + 1}]
 
-mol modselect $irep $imol not (water or ions)
-mol modstyle $irep $imol licorice 0.300000 10.000000 3.000000 0
-mol modcolor $irep $imol ColorID $silver
+mol addrep $imol
+mol modselect $irep $imol nucleic
+mol modstyle $irep $imol Licorice 0.200000 10.000000 10.000000
+mol modcolor $irep $imol Name
 set irep [expr {$irep + 1}]
 
 mol addrep $imol
 mol modselect $irep $imol resname DA DT
-mol modstyle $irep $imol Licorice 0.300000 10.000000 10.000000
+mol modstyle $irep $imol Licorice 0.200000 10.000000 10.000000
 mol modcolor $irep $imol Name
+set irep [expr {$irep + 1}]
+
+
+mol addrep $imol
+mol modselect $irep $imol (resname DA and name N1) or (resname DT and name N3)
+mol modstyle $irep $imol VdW 0.600000 12.000000
+mol modcolor $irep $imol ColorId $green
 set irep [expr {$irep + 1}]
