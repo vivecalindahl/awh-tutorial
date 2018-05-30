bf471ee1d61245f8baf934fa75a6cd4b8f2526aa
Viveca Lindahl <vivecalindahl@gmail.com>
2018-05-30 10:52:31 +0200

    tmp commit

--- dna-base-pair-opening/visualization/representation.tcl
+++ dna-base-pair-opening/visualization/representation.tcl
@@ -3,28 +3,33 @@ set gray 2
 set silver 6
 set green 7
 
-set irep 0
+# The molecule index (usually just one)
 set imol 0
 
-# Nucleic acid backbone              
+# The representation index
+set irep 0
+
+# First representation: nucleic acid backbone              
 mol modselect $irep $imol nucleic and backbone
 mol modstyle $irep $imol NewRibbons 0.300000 10.000000 3.000000 0
 mol modcolor $irep $imol ColorID $silver
 set irep [expr {$irep + 1}]
 
+# Make another representation: all nucleic atoms
 mol addrep $imol
 mol modselect $irep $imol nucleic
 mol modstyle $irep $imol Licorice 0.200000 10.000000 10.000000
 mol modcolor $irep $imol Name
 set irep [expr {$irep + 1}]
 
+# All A, T bases
 mol addrep $imol
 mol modselect $irep $imol resname DA DT
 mol modstyle $irep $imol Licorice 0.200000 10.000000 10.000000
 mol modcolor $irep $imol Name
 set irep [expr {$irep + 1}]
 
-
+# The nitrogens forming the central Watson-Crick H-bond in AT base pairs.
 mol addrep $imol
 mol modselect $irep $imol (resname DA and name N1) or (resname DT and name N3)
 mol modstyle $irep $imol VdW 0.600000 12.000000
